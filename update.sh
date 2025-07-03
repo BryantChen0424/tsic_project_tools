91dc737a13be76d7c389650730a92d209c7aa520
verilog <bryant90424@gmail.com>
2025-07-03 02:18:34 -0700

    25_0703_15

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 echo "update projectV"
-cp ./projectV.desktop /home/verilog/.local/share/applications/
+cp $DLAB_ROOT/.private/projectV_dev/projectV.desktop /home/verilog/.local/share/applications/
 update-desktop-database ~/.local/share/applications
 desktop-file-validate ~/.local/share/applications/projectV.desktop
 killall -SIGUSR1 gnome-shell
